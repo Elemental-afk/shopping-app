4fe7e1ec9f1bf03652e9026e007f5aac6e8de557
MajorLimitowny <MajorLimitowny@users.noreply.github.com>
2020-10-25 00:07:04 +0200

    Finalny Update skryptów na dziś

--- Shopping App - Unity/Assets/Scripts/Produkty.cs
+++ Shopping App - Unity/Assets/Scripts/Produkty.cs
@@ -7,18 +7,23 @@ public class Produkty : MonoBehaviour
     //float kanapka = 5f;
     //float kanapkaXl = 6.5f;
     public static float cartSum = 0f;
-    float kan = 0f;
-    float kanXl = 0f;
-    public static int[] prod = {0, 0,};
-    static float[] cena = { 5f, 6.5f, };
-    static string[] nazwa = {"kanapka","kanapkaXL",};
-    public static float[] xProduktu = { 0, 0, };
-
+    public static float[] prod = {0f, 0f, 0f, 0f,};
+    static float[] cena = { 5f, 6.5f, 1.7f, 3f,};
+    public static float[] xProduktu = { 0f, 0f, 0f, 0f,};
+    public static string[] nazwa = {"Kanapka", "KanapkaXL", "Batonik", "Tymbark",};
     public static void Sum()
     {
-        cartSum = prod[0] * cena[0] + prod[1] * cena[1];
-        xProduktu[0] = prod[0] * cena[0];
-        xProduktu[1] = prod[1] * cena[1];
+        cartSum = 0f;
+        for (int x = 0; x < 4; x++)
+        {
+            xProduktu[x] = prod[x] * cena[x];
+            cartSum += xProduktu[x];
+            //cartSum = prod[0] * cena[0] + prod[1] * cena[1];
+            //xProduktu[0] = prod[0] * cena[0];
+            //xProduktu[1] = prod[1] * cena[1];
+            //Debug.Log(prod[x]);
+        }
         Debug.Log(cartSum);
+        
     }
 }
--- Shopping App - Unity/Assets/Scripts/TextKoszyka.cs
+++ Shopping App - Unity/Assets/Scripts/TextKoszyka.cs
@@ -11,13 +11,29 @@ public class TextKoszyka : MonoBehaviour
     void Update()
     {
         rozpiska.text = "Produkty w koszyku:";
-        if (Produkty.prod[0] > 0)
+        for (int x = 0; x < 4; x++)
+        {
+            if (Produkty.prod[x] > 0)
+            {
+                rozpiska.text += Environment.NewLine + Produkty.nazwa[x] + " x" + Produkty.prod[x] + " " + Produkty.xProduktu[x] + "zł";
+            }
+        }
+        rozpiska.text += Environment.NewLine + "Całkowita wartość koszyka: " + Produkty.cartSum + "zł";
+        /*if (Produkty.prod[0] > 0)
         {
             rozpiska.text += Environment.NewLine + "Kanapka x" + Produkty.prod[0] + " " + Produkty.xProduktu[0] + "zł";
         }
         if (Produkty.prod[1] > 0)
         {
-            rozpiska.text += Environment.NewLine + "KanapkaXL x" + Produkty.prod[0] + " " + Produkty.xProduktu[1] + "zł";
+            rozpiska.text += Environment.NewLine + "KanapkaXL x" + Produkty.prod[1] + " " + Produkty.xProduktu[1] + "zł";
         }
+        if (Produkty.prod[2] > 0)
+        {
+            rozpiska.text += Environment.NewLine + "Batonik x" + Produkty.prod[2] + " " + Produkty.xProduktu[2] + "zł";
+        }
+        if (Produkty.prod[3] > 0)
+        {
+            rozpiska.text += Environment.NewLine + "Tymbark x" + Produkty.prod[3] + " " + Produkty.xProduktu[3] + "zł";
+        } */
     }
 }
